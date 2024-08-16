cf50d107b0e71c2394506b43d99ecc426a3ba7cc
elmoulaouianouar <elmoulaoui.anouar@gmail.com>
2024-08-16 20:03:44 +0100

    finish app

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -4,10 +4,17 @@ import { createSlice } from '@reduxjs/toolkit';
 export const mealsSlice = createSlice({
   name: 'meals',
   initialState: [
-   
+    { name: 'Breakfast', cost: 50, selected: false },
+    { name: 'High Tea', cost: 25, selected: false },
+    { name: 'Lunch', cost: 65, selected: false },
+    { name: 'Dinner', cost: 70, selected: false },
   ],
   reducers: {
     toggleMealSelection: (state, action) => {
+      const {payload} = action
+      if(state[payload]){
+         state[payload].selected = !state[payload].selected;
+      }
     },
   },
 });
--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
+import addOnsReducer from './avSlice';
+import mealsReducer from './mealsSlice';
 import venueReducer from './venueSlice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    addons : addOnsReducer,
+    meals : mealsReducer ,
   },
 });
